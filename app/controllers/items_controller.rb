8d8327a4e78a32caf95758d5272e0c2e5015d854
kabomDev <kabom.y@yahoo.com>
2018-08-28 18:52:41 +0200

    template show fini et ajout du bouton dans index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-  	@item = Item.find(params[:id])
+  	#@item = Item.find(params[:id])
   end
+	
+	private
+		def items_params
+			params.require(:item).permit(:title, :description, :price, :image_url)
+		end
 end
