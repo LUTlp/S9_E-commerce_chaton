e9efcf94ad5a177fd85ce38495b041d9cb642a88
Koz-Koz <kozjulia@hotmail.fr>
2018-08-29 15:05:31 +0200

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   root 'items#index'
-  # get '/items/:id', to: 'items#show', as: 'item' 
-resources :items, only: [:show]
+  get '/items/:id', to: 'items#show', as: 'item' # resources :items, only: [:show]
+
   devise_for :users
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htm
